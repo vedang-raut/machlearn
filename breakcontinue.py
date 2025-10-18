ace952b0dbd59c47d8a30c5cacc0e4857c5ace4c
Vedang <vedangraut1123@gmail.com>
2025-10-19 02:02:44 +0530

    second commit

---
+++ breakcontinue.py
@@ -0,0 +1,17 @@
+'''count=0
+while count<=5:
+    if count==2:
+        print("loop broke")
+        continue
+    else:
+        print(count)
+    count=count+1'''
+
+
+count=0
+while count<=10:
+    if count==3:
+        count=count+1
+        continue
+    print("hell",count)
+    count=count+1
