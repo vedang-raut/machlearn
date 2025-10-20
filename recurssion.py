8efb33d0dca2b34dcfa1e761b163178fa63e6c29
Vedang <vedangraut1123@gmail.com>
2025-10-20 05:39:16 +0530

    The last commit of the day, Good night :)

--- recurssion.py
+++ recurssion.py
@@ -1,4 +1,4 @@
-def show(n):
+'''def show(n):
     if n==0:
         return
     print(n)
@@ -8,3 +8,57 @@ def show(n):
     
 
 show(5)
+
+
+
+
+
+
+def factorial(n):
+    if (n==0 or n==1):
+        return 1
+    else:
+        return n*factorial(n-1)
+    
+
+    
+
+
+
+print(factorial(5))
+   
+
+
+
+def natural_sum(n):
+    if n==0:
+        return 0
+    else:
+        return natural_sum(n-1)+n
+        print("end")
+    
+      
+ 
+print(natural_sum(5)) 
+
+
+
+def bmw(list,index=0):
+    if index==len(list):
+        return
+    else:
+        print(list[index])
+        bmw(list, index+1)
+    
+cars=["venue","audi","hyundai","sonet","kia"]    
+(bmw(cars))
+
+
+''' 
+
+def natural (n):
+    if n==0:
+        return 
+    print(n)
+    natural(n-1)
+natural(5)
