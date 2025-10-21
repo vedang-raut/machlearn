5cf92af9148fbbc8d4f80a30a687e8c29cff54cf
Vedang <vedangraut1123@gmail.com>
2025-10-21 14:38:06 +0530

    all files commited

--- file handling.py
+++ file handling.py
@@ -31,7 +31,70 @@ data=hang.write("I have rewritten all the text")
 print(data)
 
 
-'''
+
 f=open("demo.txt","at")
 data=f.write("\n this is append")
 print(data)
+
+
+
+f=open("demo.txt","w+")
+data=f.write("sanika")
+print(data)
+
+
+
+f=open("demo.txt","r+")
+data=f.write("they print in the front, as the cursor is ended")
+vedang=f.write("\n now i")
+sanika=f.write("doctor hai bhai voh")
+
+print(data)
+print(vedang)
+
+
+
+
+
+f=open("demo.txt","a+")
+datas=f.read()
+data=f.write("vedang")
+datas=f.read()
+
+print(data)
+print(datas)
+
+
+
+with open("vedang.txt","x") as f:
+    print(f.read())
+
+
+f=open("sample.txt","w")
+print(f.write("this will be deleted"))
+
+import os
+os.remove("sample.txt")
+
+
+
+
+
+with open("reagaltos.txt", 'x') as f:
+    f.write("vx")
+
+'''
+
+with open("practice.txt","r") as f:
+    data=f.read()
+newdata=data.replace("java","python")
+print(newdata)  
+
+
+with open("practice.txt","w") as f:
+    f.write(newdata)
+
+
+
+   
+    
---
+++ oops.py
@@ -0,0 +1,6 @@
+class cars:
+    model="mazda"
+    color="cherry red"
+cars1=cars()
+print(cars1.model)
+print(cars1.color)
